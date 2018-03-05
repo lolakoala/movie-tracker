3a831922ec7682dd2c2e55252f1415d9e2c5e74b
Lola Brenner <amandadbrenner@gmail.com>
2018-03-05 12:20:19 -0700

    adds condition for production env variable

--- routes/queries.js
+++ routes/queries.js
@@ -5,7 +5,7 @@ var options = {
 };
 
 var pgp = require('pg-promise')(options);
-var connectionString = 'postgres://localhost:5432/users';
+var connectionString = process.env.NODE_ENV === 'production' ? process.env.DATABASE_URL + `?ssl=true` :'postgres://localhost:5432/users';
 var db = pgp(connectionString);
 
 function getAllUsers(req, res, next) {
